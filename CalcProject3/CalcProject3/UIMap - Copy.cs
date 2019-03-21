40c8d0eb23fc7b3f289c98f6572e878e60b7bd37
NMIN66587-PC <nmsibackup@gmail.com>
2019-03-21 13:55:48 +0530

    This is my First Branch commit

---
+++ CalcProject3/CalcProject3/UIMap - Copy.cs
@@ -0,0 +1,24 @@
+﻿namespace CalcProject3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Windows.Input;
+    using System.CodeDom.Compiler;
+    using System.Text.RegularExpressions;
+    using Microsoft.VisualStudio.TestTools.UITest.Extension;
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.TestTools.UnitTesting;
+    using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+    using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
+    using MouseButtons = System.Windows.Forms.MouseButtons;
+    using System.io;
+    
+    public partial class UIMap
+    {
+		public void main(string args[])
+		{
+			Console.writeline("Sample");
+		}
+    }
+}
